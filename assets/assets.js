0be144a9780e37c603816a98827c4943f9974d8d
Fahad Ali <fahad3980905@gmail.com>
2025-04-13 00:16:50 +0500

    chnges

--- assets/assets.js
+++ assets/assets.js
@@ -157,7 +157,7 @@ export const serviceData = [
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML5, CSS3, JavaScript, Liquid, Jquery, Ajax, Shopify CLI, Git' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 3 projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 6 projects' }
 ];
 
 export const toolsData = [
